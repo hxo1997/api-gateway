9b97cdcf7934e218d96ef9f57869acfb6dfbfc57
hxo1997 <thebkviphxo@gmail.com>
2017-07-10 12:13:39 +0700

    g

--- api.go
+++ api.go
@@ -30,7 +30,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 func notFound(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Cai gi cung dc")
+	fmt.Fprintln(w, "Cai gi cung dc la cai gi abcddd")
 	log.Println(r.Method + " - " + r.URL.Path)
 }
 func testAPI(w http.ResponseWriter, r *http.Request) {
